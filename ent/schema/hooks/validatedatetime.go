41967d55a306a57527cdbb14b7e61a3e92965ab3
Hendrik Schellenberger <hkonitzer@googlemail.com>
2023-11-11 14:57:29 +0100

    disable ent.OpUpdate|ent.OpUpdateOne

--- ent/schema/hooks/validatedatetime.go
+++ ent/schema/hooks/validatedatetime.go
@@ -31,11 +31,14 @@ func EnsureOnlyOneTimetableEntry() ent.Hook {
 				),
 				timetable.TimetableTypeEQ(ty),
 				timetable.DatetimeToLTE(to),
+				timetable.DeletedAtIsNil(),
 			).All(ctx)
 			if err != nil && !ent.IsNotFound(err) {
 				return nil, err
 			}
-
+			// address_timetables is NIL? why?
+			// cannot check against given address_id (aid)
+			// TODO: get address_timetables from timetable entry and enable this check also for ent.OpUpdate|ent.OpUpdateOne
 			// check if datetime_to is in range of another entry
 			for _, tt := range tts {
 				if to.After(tt.DatetimeTo) && !f.Before(tt.DatetimeTo) {
@@ -51,5 +54,5 @@ func EnsureOnlyOneTimetableEntry() ent.Hook {
 			return next.Mutate(ctx, m)
 		})
 	}
-	return hook.On(hk, ent.OpCreate|ent.OpUpdate|ent.OpUpdateOne)
+	return hook.On(hk, ent.OpCreate)
 }
